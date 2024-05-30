70aa3b2cad495308cbfdf94a2c795528afb49004
aliwdg16 <alialtoto@hotmail.com>
2024-05-30 11:17:41 +0200

    bevor pullen 11:17 30.05.2024

--- frontend/src/component/Mainpage.jsx
+++ frontend/src/component/Mainpage.jsx
@@ -17,15 +17,27 @@ const Mainpage = () => {
     
     {/* navbar */}
     <div className=" flex  items-center justify-center flex-wrap mt-20  min-w-[50%]">
-    <ul className="my-2 flex flex-row flex-wrap  bg-[#d7423b] px-[20%] ">
+    <ul className="my-2 flex flex-row flex-wrap  bg-[#d7423b] px-[20%] rounded-xl ">
      <Link to={`/`} > <Typography
         as="li"
         variant="small"
         color="blue-gray"
         className="p-1 font-medium text-2xl"
       >
-        <a href="#" className="flex items-center mr-6 hover:text-blue-500 transition-colors">
+        <a href="#" className="flex items-center mr-6 hover:text-[#b5bcc7] transition-colors">
         Home
+        </a>
+      </Typography>
+      </Link>
+    <Link to={"/leaderboard"}>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium text-2xl"
+      >
+        <a href="#" className="flex items-center mr-6 hover:text-[#b5bcc7] transition-colors">
+        Leaderboard
         </a>
       </Typography>
       </Link>
@@ -35,17 +47,7 @@ const Mainpage = () => {
         color="blue-gray"
         className="p-1 font-medium text-2xl"
       >
-        <a href="#" className="flex items-center mr-6 hover:text-blue-500 transition-colors">
-        Leaderboard
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-medium text-2xl"
-      >
-        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+        <a href="#" className="flex items-center hover:text-[#b5bcc7]  transition-colors">
         instruction
         </a>
       </Typography>
@@ -58,20 +60,24 @@ const Mainpage = () => {
         <div className=" flex flex-col items-center mt-8">
          
           <Card />
-          <img
+          <button> <img
             src="./src/assets/pokeball.webp"
             className=" h-[20rem] w-[17rem] mt-9"
-          />
+          /></button> 
         </div>
-        <div className=" mt-80"> <button><img src="./src/assets/Schwert.webp" className=" h-[20rem] w-[17rem] mt-9" /></button> </div>
+        <div className=" mt-60"> 
+        {/* <button><img src="./src/assets/Schwert.webp" className=" h-[20rem] w-[17rem] mt-9" alt="Fight" /></button> */}
+
+        <iframe width="110" height="200" src="https://www.myinstants.com/instant/whos-that-pokemon-90928/embed/" frameborder="0" scrolling="no"></iframe>
+         </div>
 
         <div className="flex flex-col items-center mt-8">
       
           <Card />
-          <img
+          <button> <img
             src="./src/assets/pokeball.webp"
             className=" h-[20rem] w-[17rem] mt-9"
-          />
+          /></button> 
         </div>
       </section>
     </div>
